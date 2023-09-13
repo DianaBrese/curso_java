f206e1a049d52c26fae6b2150134aaf214356889
Diana <dianabrese15@gmail.com>
2023-09-13 09:26:58 -0300

    Exercicio membros estáticos

---
+++ curso_java/src/anotacoes/Main.java
@@ -0,0 +1,18 @@
+package anotacoes;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		
+		
+		
+		sc.close();
+
+	}
+	
+
+}
